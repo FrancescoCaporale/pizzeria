ddbd61b4ebb10dcccc9043f62216df9ad9b5f476
FrancescoCaporale <frenk.caporale@gmail.com>
2026-08-23 15:51:02 +0200

    fatto array con pizze

---
+++ src/data/pizzas.js
@@ -0,0 +1,64 @@
+const pizzas = [
+    {
+        id: 1,
+        name: "Margherita",
+        description: "Pomodoro, fiordilatte e basilico",
+        price: 8,
+    },
+    {
+        id: 2,
+        name: "Diavola",
+        description: "Pomodoro, fiordilatte e salame piccante",
+        price: 10,
+    },
+    {
+        id: 3,
+        name: "Bufalina",
+        description: "Pomodoro, mozzarella di bufala e basilico",
+        price: 11,
+    },
+    {
+        id: 4,
+        name: "Quattro Formaggi",
+        description: "Fiordilatte, gorgonzola, parmigiano e fontina",
+        price: 11,
+    },
+    {
+        id: 5,
+        name: "Capricciosa",
+        description: "Pomodoro, fiordilatte, prosciutto, funghi, olive e carciofi",
+        price: 12,
+    },
+    {
+        id: 6,
+        name: "Prosciutto e Funghi",
+        description: "Pomodoro, fiordilatte, prosciutto cotto e funghi",
+        price: 10,
+    },
+    {
+        id: 7,
+        name: "Boscaiola",
+        description: "Fiordilatte, salsiccia, funghi e mozzarella",
+        price: 12,
+    },
+    {
+        id: 8,
+        name: "Parmigiana",
+        description: "Pomodoro, fiordilatte, melanzane, parmigiano e basilico",
+        price: 11,
+    },
+    {
+        id: 9,
+        name: "Mortadella e Pistacchio",
+        description: "Fiordilatte, mortadella, pistacchio e stracciatella",
+        price: 13,
+    },
+    {
+        id: 10,
+        name: "Tonno e Cipolla",
+        description: "Pomodoro, fiordilatte, tonno e cipolla rossa",
+        price: 10,
+    },
+];
+
+export default pizzas;
